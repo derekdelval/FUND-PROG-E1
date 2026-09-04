3315f0ee872a2b5d5984044ac6153290e19630ec
derekdelval <L26550296@chihuahua2.TECNM.MX>
2026-09-03 18:51:20 -0600

    Add files via upload

---
+++ EVA1_1_hola_mundo/src/eva1_1_hola_mundo/EVA1_1_hola_mundo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_1_hola_mundo;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_1_hola_mundo {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    //INICIO DEL PROGRAMA
+
+// TODO code application logic here
+
+System.out.println("Hola Mundo");
+  }
+  
+}
