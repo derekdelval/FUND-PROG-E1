bc7b069924ef347873779194d9f720f7779a3f9d
derekdelval <L26550296@chihuahua2.TECNM.MX>
2026-09-14 18:53:08 -0600

    Add files via upload

---
+++  EVA1_8_OPERACIONES_2/src/eva1_8_operaciones_2/EVA1_8_OPERACIONES_2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_operaciones_2;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_8_OPERACIONES_2 {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+   
+    int resu, val1 = 10, val2 = 5; // declaracion e INICIALIZANDO
+    
+    //JAVA ES BUENA ONDA: INICIALIZA EN AUTOMATICO LAS VARIABLES.
+    // NUMERO EN CERO, BOOLEANOS EN FALSO
+    
+    resu = 100 + val1 * (val1 / val2);
+       
+       System.out.print("Resu = ");
+       System.out.println(resu);
+    
+    
+  }
+  
+}
